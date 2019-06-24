e92fed1fd35a4ed020001aef730ec6d22e1c88d8
Minggl <guangliang1120@gmail.com>
2019-06-24 15:54:38 +0800

    [TECH][Monk] add consumer-hystrix-dashboard

---
+++ consumer-hystrix-dashboard/src/main/java/com/github/mgljava/consumerhystrixdashboard/consumerhystrixdashboard/ConsumerHystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.github.mgljava.consumerhystrixdashboard.consumerhystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerHystrixDashboardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumerHystrixDashboardApplication.class, args);
+  }
+
+}
