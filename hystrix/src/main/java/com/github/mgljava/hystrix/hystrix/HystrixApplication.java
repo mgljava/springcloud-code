80372283be41709759247153ba9d057aad165e3c
Minggl <guangliang1120@gmail.com>
2019-06-24 15:50:43 +0800

    [TECH][Monk] 服务器端熔断

--- hystrix/src/main/java/com/github/mgljava/hystrix/hystrix/HystrixApplication.java
+++ hystrix/src/main/java/com/github/mgljava/hystrix/hystrix/HystrixApplication.java
@@ -2,7 +2,9 @@ package com.github.mgljava.hystrix.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class HystrixApplication {
 
