558be69886c98d1e717e949a20f54e4c5c1b11c5
Minggl <guangliang1120@gmail.com>
2019-06-24 17:06:50 +0800

    [TECH][Monk] add config-server

---
+++ config-center-consumer/src/test/java/com/github/mgljava/configcenterconsumer/configcenterconsumer/ConfigCenterConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.mgljava.configcenterconsumer.configcenterconsumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigCenterConsumerApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
